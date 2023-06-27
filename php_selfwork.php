7913fc01df7548504d43a79cf2a8d9150249b8d8
Claudio-Paradisi <claudio.paradisi94@gmail.com>
2023-06-27 20:07:10 +0200

    esercizio 2

--- php_selfwork.php
+++ php_selfwork.php
@@ -9,10 +9,10 @@ $bool = true;
 $string = "I'm gonna be the next king of the pirate";
 
 //stampo tipo di dato
-var_dump($int);
-var_dump($float);
-var_dump($bool);
-var_dump($string);
+// var_dump($int);
+// var_dump($float);
+// var_dump($bool);
+// var_dump($string);
 
 //converto in costanti
 
@@ -23,11 +23,25 @@ const FLOATT = 3.2;
 const BOOLEANO = false;
 const STRINGA = "I'm gonna be the nest king of the pirates";
 
-var_dump(INTEGERR);
-var_dump(FLOATT);
-var_dump(BOOLEANO);
-var_dump(STRINGA);
+// var_dump(INTEGERR);
+// var_dump(FLOATT);
+// var_dump(BOOLEANO);
+// var_dump(STRINGA);
 
+
+//esercizio 2
+
+
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+//  lo cancello perche abbiamo gia variabile con stesso valore $text6 = '$text2';
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+var_dump("$text1 $text2 $text3$text4 $text5 $text2 $text7 $text8.");
 
 
 
