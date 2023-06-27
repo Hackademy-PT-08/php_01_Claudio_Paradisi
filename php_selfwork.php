e8f3195c7b705cdbc13992a69c5ed10ca4d6e519
Claudio-Paradisi <claudio.paradisi94@gmail.com>
2023-06-27 19:56:11 +0200

    esercizio1

---
+++ php_selfwork.php
@@ -0,0 +1,39 @@
+<?php 
+
+//esercizio 1
+
+//creo le 4 variabili
+$int = 6;
+$float = 3.2;
+$bool = true;
+$string = "I'm gonna be the next king of the pirate";
+
+//stampo tipo di dato
+var_dump($int);
+var_dump($float);
+var_dump($bool);
+var_dump($string);
+
+//converto in costanti
+
+//non riesco le creo da 0
+
+const INTEGERR = 6;
+const FLOATT = 3.2;
+const BOOLEANO = false;
+const STRINGA = "I'm gonna be the nest king of the pirates";
+
+var_dump(INTEGERR);
+var_dump(FLOATT);
+var_dump(BOOLEANO);
+var_dump(STRINGA);
+
+
+
+
+
+
+
+
+
+
